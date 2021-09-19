2546504ed7cacb1859c519e0c57e3021de866717
FelixBucket <FelixBucket@users.noreply.github.com>
2021-09-19 17:16:02 -0600

    Sprinting

--- toontown/toonbase/ToonBase.py
+++ toontown/toonbase/ToonBase.py
@@ -130,6 +130,9 @@ class ToonBase(OTPBase.OTPBase):
         del tpMgr
         self.lastScreenShotTime = globalClock.getRealTime()
         self.accept('InputState-forward', self.__walking)
+        self.accept('shift', self.setSprinting)
+        self.accept('shift-up', self.exitSprinting)
+        self.isSprinting = 0
         self.canScreenShot = 1
         self.glitchCount = 0
         self.walking = 0
@@ -138,6 +141,25 @@ class ToonBase(OTPBase.OTPBase):
         self.aspectRatio = float(self.oldX) / self.oldY
         self.aspect2d.setAntialias(AntialiasAttrib.MMultisample)
         return
+
+    def setSprinting(self):
+        if hasattr(base, 'localAvatar'):
+            base.localAvatar.currentSpeed = OTPGlobals.ToonForwardSprintSpeed
+            base.localAvatar.currentReverseSpeed = OTPGlobals.ToonReverseSprintSpeed
+            base.localAvatar.controlManager.setSpeeds(OTPGlobals.ToonForwardSprintSpeed, OTPGlobals.ToonJumpForce,
+                                                      OTPGlobals.ToonReverseSprintSpeed, OTPGlobals.ToonRotateSpeed)
+            self.isSprinting = 1
+        else:
+            if self.isSprinting == 1:
+                self.exitSprinting()
+
+    def exitSprinting(self):
+        if hasattr(base, 'localAvatar'):
+            base.localAvatar.currentSpeed = OTPGlobals.ToonForwardSpeed
+            base.localAvatar.currentReverseSpeed = OTPGlobals.ToonReverseSpeed
+            base.localAvatar.controlManager.setSpeeds(OTPGlobals.ToonForwardSpeed, OTPGlobals.ToonJumpForce,
+                                                      OTPGlobals.ToonReverseSpeed, OTPGlobals.ToonRotateSpeed)
+            self.isSprinting = 0
 
     def openMainWindow(self, *args, **kw):
         result = OTPBase.OTPBase.openMainWindow(self, *args, **kw)
