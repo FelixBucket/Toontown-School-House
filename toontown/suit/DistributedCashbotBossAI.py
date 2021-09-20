c50f1b9435ce1d498eaf7f6f09ec3dfe21006fc9
FelixBucket <FelixBucket@users.noreply.github.com>
2021-09-20 12:35:45 -0600

    EXEs in CFO

--- toontown/suit/DistributedCashbotBossAI.py
+++ toontown/suit/DistributedCashbotBossAI.py
@@ -59,11 +59,12 @@ class DistributedCashbotBossAI(DistributedBossCogAI.DistributedBossCogAI, FSM.FS
         self.initializeBattles(1, ToontownGlobals.CashbotBossBattleOnePosHpr)
 
     def generateSuits(self, battleNumber):
-        cogs = self.invokeSuitPlanner(11, 0, 0)
-        skelecogs = self.invokeSuitPlanner(12, 1, 0)
-        executives = self.invokeSuitPlanner(13, 0, 1)
-        activeSuits = cogs['activeSuits'] + skelecogs['activeSuits'] + executives['activeSuits']
-        reserveSuits = cogs['reserveSuits'] + skelecogs['reserveSuits'] + executives['reserveSuits']
+        cogs = self.invokeSuitPlanner(11, 0)
+        executives = self.invokeSuitPlanner(21, 0, 1)
+        skelecogs = self.invokeSuitPlanner(12, 1)
+        skelecogExecutives = self.invokeSuitPlanner(22, 1, 1)
+        activeSuits = cogs['activeSuits'] + skelecogs['activeSuits'] + executives['activeSuits'] + skelecogExecutives['activeSuits']
+        reserveSuits = cogs['reserveSuits'] + skelecogs['reserveSuits'] + executives['reserveSuits'] + skelecogExecutives['reserveSuits']
         random.shuffle(activeSuits)
         while len(activeSuits) > 4:
             suit = activeSuits.pop()
@@ -75,28 +76,6 @@ class DistributedCashbotBossAI(DistributedBossCogAI.DistributedBossCogAI, FSM.FS
         reserveSuits.sort(compareJoinChance)
         return {'activeSuits': activeSuits,
          'reserveSuits': reserveSuits}
-
-    def handleRoundDone(self, battle, suits, activeSuits, toonIds, totalHp, deadSuits):
-        totalMaxHp = 0
-        for suit in suits:
-            totalMaxHp += suit.maxHP
-
-        for suit in deadSuits:
-            activeSuits.remove(suit)
-
-        joinedReserves = []
-        if len(self.reserveSuits) > 0 and len(activeSuits) < 6:
-            hpPercent = 100 - totalHp / totalMaxHp * 100.0
-            for info in self.reserveSuits:
-                if info[1] <= hpPercent and len(activeSuits) < 6:
-                    suits.append(info[0])
-                    activeSuits.append(info[0])
-                    joinedReserves.append(info)
-
-            for info in joinedReserves:
-                self.reserveSuits.remove(info)
-
-        battle.resume(joinedReserves)
 
     def removeToon(self, avId):
         if self.cranes != None:
